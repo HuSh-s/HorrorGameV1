30b981144eadfaa5cb6f734ae243be5a329c072b
HuSh-s <semihbay17@gmail.com>
2025-06-23 21:55:34 +0300

    Added Compass and Basic Dialogue System
    - Implemented compass UI that rotates based on player Y-axis rotation
    - Added dialogue trigger system that displays text when player enters the area
    - Dialogue text automatically hides after a delay and removes activator object

---
+++ Assets/Scripts/CompassImage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassImage : MonoBehaviour
+{
+    public Transform player;
+    Vector3 dir;
+    void Update()
+    {
+        dir.z = player.eulerAngles.y;
+        transform.localEulerAngles = dir;
+    }
+}
